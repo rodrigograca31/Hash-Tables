9cea3a62c158b044c956a398b14a571dec84e600
Rodrigo Graça <rodrigograca31@gmail.com>
2020-05-07 14:58:52 +0100

    feat(lookup_table): ✨ lookup table implmentation

--- applications/lookup_table/lookup_table.py
+++ applications/lookup_table/lookup_table.py
@@ -1,19 +1,30 @@
+import time
 import math
 import random
 
+cache = {}
+
+
 def slowfun(x, y):
+
+    if str(x)+"_"+str(x) in cache.keys():
+        return cache[str(x)+"_"+str(x)]
     # TODO: Modify to produce the same results, but much faster
     v = math.pow(x, y)
     v = math.factorial(v)
     v //= (x + y)
     v %= 982451653
-
+    cache[str(x)+"_"+str(x)] = v
     return v
 
 
 # Do not modify below this line!
 
-for i in range(50000):
+start = time.time()
+
+for i in range(100000):
     x = random.randrange(2, 14)
     y = random.randrange(3, 6)
     print(f'{i}: {x},{y}: {slowfun(x, y)}')
+
+print("Imp 1: \t%.8f" % float(time.time() - start))
