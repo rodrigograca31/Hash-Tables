a86538197e08cfb10cfd4ac2b9a9e553c949ce84
Rodrigo Graça <rodrigograca31@gmail.com>
2020-05-07 15:45:32 +0100

    feat: ✨ expensive sequence

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,9 +1,31 @@
-def expensive_seq(x, y, z):
+import time
+cache = {}
+
+
+def exps(x, y, z):
     # Implement me
 
+    # use a tuple as the Key
+    if (x, y, z) in cache.keys():
+        return cache[(x, y, z)]
+
+    result = None
+    if x <= 0:
+        result = y + z
+    if x > 0:
+        result = exps(x-1, y+1, z) + exps(x-2, y+2, z*2) + exps(x-3, y+3, z*3)
+
+    # use a tuple as the Key
+    cache[(x, y, z)] = result
+    return result
+
+
 if __name__ == "__main__":
-    for i in range(10):
-        x = expensive_seq(i*2, i*3, i*4)
+    start = time.time()
+
+    for i in range(100):
+        x = exps(i*2, i*3, i*4)
         print(f"{i*2} {i*3} {i*4} = {x}")
 
-    print(expensive_seq(150, 400, 800))
+    print(exps(150, 400, 800))
+    print("Imp 1: \t%.8f" % float(time.time() - start))
